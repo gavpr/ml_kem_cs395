ec0924b480c8d4f38552a1ef63f61a062c4fe826
gavpr <jamesgprewitt@outlook.com>
2026-03-03 00:26:12 -0500

    update params.h

---
+++ include/params.h
@@ -0,0 +1,16 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int32_t i32;
+typedef int32_t i32;
+
+#endif
+
