66df0ed21a28879ce276ead3e68d7e39b2bd3387
gavpr <jamesgprewitt@outlook.com>
2026-03-03 00:33:08 -0500

    changes

--- include/mlkem.h
+++ include/mlkem.h
@@ -1,2 +1,14 @@
 #include <stdio.h>
-#include <stdint.h>
+#include "params.h"
+
+int mlkem512_keygen();
+int mlkem512_encaps();
+int mlkem512_decaps();
+
+int mlkem768_keygen();
+int mlkem768_encaps();
+int mlkem768_decaps();
+
+int mlkem1024_keygen();
+int mlkem1024_encaps();
+int mlkem1024_decaps();
