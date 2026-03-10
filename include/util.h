2f79e57b2e07a7e5f485be867b3edfce778e0e1c
gavpr <jamesgprewitt@outlook.com>
2026-03-09 20:16:07 -0400

    update util

--- include/util.h
+++ include/util.h
@@ -11,10 +11,18 @@
 //compress
 //decompress
 
-// convert a bit array of length l*8 into array of bytes
+// convert a bit array into array of bytes
 void bits_to_bytes(const uint8_t* b, uint8_t* B, int l);
 
-//convert a byte array of length l*8 into an array of bits
+//convert a byte array into an array of bits
 void bytes_to_bits(const uint8_t* B, uint8_t* b, int l);
+
+//encodes array of d-bit integers into a byte array for 1 <= d <= 12
+//output B is length 32*d bytes
+void byte_encode(const uint16_t F[ML_KEM_N], uint8_t* B, uint8_t d);
+
+//decodes a byte array into an array of d-bit integers for 1 <= d <= 12
+//again input byte array (B) should be length 32*d
+void byte_decode(const uint8_t* B, uint16_t F[ML_KEM_N], uint8_t d);
 
 #endif
